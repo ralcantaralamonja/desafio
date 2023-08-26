48946807abcbe08929a5e55167c657ea108f339a
Ricardo Alcantara <alcantararicardo04@gmail.com>
2023-08-26 14:13:59 -0500

    Pruebas unitarias completas

--- src/test/java/desafio/example/desafio/CustomerServiceTest.java
+++ src/test/java/desafio/example/desafio/CustomerServiceTest.java
@@ -1,61 +1,76 @@
 package desafio.example.desafio;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import desafio.example.desafio.controller.CustomerController;
 import desafio.example.desafio.entity.Customer;
+import desafio.example.desafio.repository.CustomerRepository;
 import desafio.example.desafio.service.CustomerService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
-
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
-@WebMvcTest(CustomerController.class)
 public class CustomerServiceTest {
 
-    @Autowired
-    private MockMvc mockMvc;
-
-    @MockBean
-    private CustomerService customerService;
+    @Mock
+    private CustomerRepository customerRepository;
 
     @InjectMocks
-    private CustomerController customerController;
-
-    private ObjectMapper objectMapper;
+    private CustomerService customerService;
 
     @BeforeEach
-    public void setup() {
-        MockitoAnnotations.initMocks(this);
-        objectMapper = new ObjectMapper();
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
-    public void testGetAllCustomers() throws Exception {
-        List<Customer> customers = new ArrayList<>();
+    public void testCreateCustomer() {
+        Customer customer = new Customer();
+        when(customerRepository.save(customer)).thenReturn(customer);
 
+        Customer createdCustomer = customerService.createCustomer(customer);
 
-        when(customerService.getEveryone()).thenReturn(customers);
-
-        mockMvc.perform(MockMvcRequestBuilders.get("/customers"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
-
+        assertEquals(customer, createdCustomer);
+        verify(customerRepository, times(1)).save(customer);
     }
 
+    @Test
+    public void testGetEveryone() {
+        List<Customer> customers = new ArrayList<>();
+        when(customerRepository.findAll()).thenReturn(customers);
 
+        List<Customer> result = customerService.getEveryone();
 
+        assertEquals(customers, result);
+        verify(customerRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void testGetCustomerById() {
+        Long customerId = 1L;
+        Customer customer = new Customer();
+        when(customerRepository.findById(customerId)).thenReturn(Optional.of(customer));
+
+        Optional<Customer> result = customerService.getCustomerById(customerId);
+
+        assertEquals(Optional.of(customer), result);
+        verify(customerRepository, times(1)).findById(customerId);
+    }
+
+    // Add more test methods for other service methods
+
+    @Test
+    public void testDeleteCustomer() {
+        Long customerId = 1L;
+
+        customerService.deleteCustomer(customerId);
+
+        verify(customerRepository, times(1)).deleteById(customerId);
+    }
 }
