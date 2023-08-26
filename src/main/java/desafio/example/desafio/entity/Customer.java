346efd26d04ba4f2b07ed3c3c834e1e76d594987
Ricardo Alcantara <alcantararicardo04@gmail.com>
2023-08-26 00:02:47 -0500

    Crud completo

--- src/main/java/desafio/example/desafio/entity/Customer.java
+++ src/main/java/desafio/example/desafio/entity/Customer.java
@@ -13,12 +13,14 @@ import java.util.Date;
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Long id;
-
+    @Column(name="first_name")
     private String firstName; // Corrección aquí
+    @Column(name="last_name")
     private String lastName;
+    @Column(name="email")
     private String email;
+    @Column(name="create_at")
     private Date createAt;
 
     // Constructores, getters, setters, etc.
